825bf36dbd0cd85965797bd951851c1e044b489b
David <mydavidkoh@gmail.com>
2015-05-18 22:06:40 -0700

    Update swap.py

--- swap.py
+++ swap.py
@@ -9,3 +9,26 @@ def swap(deck, i, j):
 	print 'swap', i, j
 	deck[i], deck[j] = deck[j], deck[i]
 	
+
+def test_shuffler(shuffler, deck='abcd', n=10000):
+	counts = defaultdict(int)
+	for _ in range(n):
+		input = list(deck)
+		shuffler(input)
+		counts[''.join(input)] += 1
+	e = n*1./factorial(len(deck))  ##expected count
+	ok = all((0.9 <= counts[item]/e <= 1.1) for item in counts)
+	name == shuffler.__name__
+	print '%s(%s) %s' % (name, deck, ('ok' if ok else '*** BAD ***'))
+	print '    ',
+	for item, count in sorted(counts.items()):
+		print "%s:%4.1f" % (item, count*100./n),
+	print
+
+def test_shufflers(shufflers=[shuffle, shuffle1], decks=['abc','ab']):
+	for deck in decks:
+		print
+		for f in shufflers:
+			test_shuffler(f, deck)
+
+def factorial(n): return 1 if (n <= 1) else n*factorial(n-1)
